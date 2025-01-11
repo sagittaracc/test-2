fbb6543ba828b78aad7600f72c3618bd400854a5
sagittaracc <sagittaracc@gmail.com>
2025-01-11 18:15:17 +0300

    добавил вывод нужных полей в перечне заявок

--- frontend/views/request/index.php
+++ frontend/views/request/index.php
@@ -25,13 +25,13 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+            // 'id',
             'name',
             'email:email',
             'status',
             'message:ntext',
-            //'comment:ntext',
-            //'created_at',
+            'comment:ntext',
+            'created_at',
             //'updated_at',
             [
                 'class' => ActionColumn::className(),
